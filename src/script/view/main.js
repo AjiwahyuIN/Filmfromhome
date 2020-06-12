18ff833813752d0394ee7aa2ddf94ee8f5a474ba
AjiWahyu <ajiewahyu24@gmail.com>
2020-06-12 23:08:33 +0700

    fix responsive jumbotron

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,34 +1,3 @@
-// $('.searchBtn').on('click', function () {
-//     $.ajax({
-//         url: "http://www.omdbapi.com/?apikey=f69c3d35&s=" + $('.keyword').val(),
-//         success: result => {
-//             const movies = result.Search;
-//             let cards = '';
-//             movies.forEach(m => {
-//                 cards += showMovies(m);
-//             });
-//             $('.cardContainer').html(cards);
-
-//             $('.detail-button').on('click', function () {
-//                 $.ajax({
-//                     url: 'http://www.omdbapi.com/?apikey=f69c3d35&i=' + $(this).data('imdbid'),
-//                     success: m => {
-//                         const movieDetail = showDetail(m);
-//                         $('.modal-body').html(movieDetail);
-//                     },
-//                     error: (e) => {
-//                         console.log(e.responText);
-//                     }
-//                 });
-//             });
-//         },
-//         error: (e) => {
-//             console.log(e.responText);
-//         }
-//     });
-// })
-
-//ketika search-button di-klik
 const searchButton = document.querySelector('.searchBtn');
 searchButton.addEventListener('click', async function () {
     try {
@@ -36,7 +5,6 @@ searchButton.addEventListener('click', async function () {
         const movies = await getMovies(inputKeyword.value);
         updateUI(movies);
     } catch (err) {
-        // console.log(err);
         alert(err);
     }
 
@@ -65,9 +33,6 @@ function updateUI(movies) {
     movieContainer.innerHTML = cards;
 }
 
-
-//ketika tombol detail di-klik
-//menagkap element yg diklik setelah element tersebut ada dgn event binding
 document.addEventListener('click', async function (e) {
     if (e.target.classList.contains('detail-button')) {
         const imdbid = e.target.dataset.imdbid;
@@ -97,7 +62,7 @@ function showMovies(m) {
         <img src="${m.Poster}" class="card-img-top">
         <div class="card-body">
             <h5 class="card-title">${m.Title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">( ${m.Year} )</h6>
             <a href="#" class="btn btn-dark text-warning detail-button" data-toggle="modal" data-target="#moviedetail" data-imdbid="${m.imdbID}">Detail</a>
         </div>
     </div>
@@ -113,7 +78,7 @@ function showDetail(m) {
         <div class="col-md">
             <ul class="list-group">
                 <li class="list-group-item">
-                    <h4>${m.Title} ${m.Year} </h4>
+                    <h4>${m.Title} ( ${m.Year} )</h4>
                 </li>
                 <li class="list-group-item"><strong>Director : </strong>${m.Director}</li>
                 <li class="list-group-item"><strong>Actor : </strong>${m.Actors}</li>
